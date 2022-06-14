d4bee1836514f4f69a4593d70ab553ab86f990bb
Erik Granda <egranda3756@uta.edu.ec>
2022-06-14 09:43:39 -0500

    Borrar la linea 15

--- src/aplicacion01/Bryan.java
+++ src/aplicacion01/Bryan.java
@@ -12,5 +12,6 @@ package aplicacion01;
 public class Bryan {
     public static void main(String[] args) {
         System.out.println("Su materia me la pela");
+        System.out.println("asdasdasdasdasdasdasdasdasdasd");
     }
 }
