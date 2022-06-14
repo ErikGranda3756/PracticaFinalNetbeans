d3c1613c532a7ffb5e5b86b015e5149a5593caa6
bryanhacker19 <bragameplayer123narufan@gmail.com>
2022-06-14 09:42:25 -0500

    Agregue una nueva clase perros

---
+++ src/aplicacion01/Bryan.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion01;
+
+/**
+ *
+ * @author UserOne
+ */
+public class Bryan {
+    public static void main(String[] args) {
+        System.out.println("Su materia me la pela");
+    }
+}
